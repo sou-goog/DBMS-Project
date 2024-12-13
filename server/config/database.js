9390baa7b3208696a7f9ccf2baba9c06fc51f518
sou-goog <sikdersouhardyo@gmail.com>
2024-12-13 23:13:01 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ const { Sequelize } = require("sequelize");
 const logStream = fs.createWriteStream('sequelize.log', {flags: 'a'});
 const DB_NAME = "BookShopDB";
 const DB_USER = "root";
-const DB_PASSWORD = "tatadottSQLadmin@21";
+const DB_PASSWORD = "dbms1234";
 const DB_HOST = "localhost"; // Replace with your host if different
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
